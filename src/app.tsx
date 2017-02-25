1338dfb8fe5cff60c05ae3103abd2b27a579d8b5
uryyyyyyy <koki305@gmail.com>
2017-02-25 22:42:40 +0900

    setup

---
+++ src/app.tsx
@@ -0,0 +1,48 @@
+
+import * as React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+const styles: any = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+export class ReactNativeAndroidSample extends React.Component<{}, {}> {
+
+  render() {
+    console.log('hello')
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit index.android.js
+        </Text>
+        <Text style={styles.instructions}>
+          Double tap R on your keyboard to reload,{'\n'}
+          Shake or press menu button for dev menu
+        </Text>
+      </View>
+    );
+  }
+}
+
