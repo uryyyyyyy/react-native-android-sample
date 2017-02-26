06d18be0a9e4f9bfc10f7a8f6cc9ae0b4487f0ba
uryyyyyyy <koki305@gmail.com>
2017-02-26 16:05:08 +0900

    kotlin

---
+++ android/app/src/main/java/com/reactnativeandroidsample/MainActivity.kt
@@ -0,0 +1,9 @@
+package com.reactnativeandroidsample
+
+import com.facebook.react.ReactActivity
+
+class MainActivity : ReactActivity() {
+    override fun getMainComponentName(): String? {
+        return "reactNativeAndroidSample"
+    }
+}
